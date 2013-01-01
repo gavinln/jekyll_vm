a55ab96fa0d239f7403d701353acec38054897e8
Gavin Noronha <gavinln@hotmail.com>
2012-12-31 18:34:58 -0800

    Improved formatting

--- puppet/manifests/classes/installrvm.pp
+++ puppet/manifests/classes/installrvm.pp
@@ -6,8 +6,7 @@ class installrvm {
     rvm::system_user { vagrant: ; }
 
     if $rvm_installed == "true" {
-        rvm_system_ruby {
-          'ruby-1.9.3-p327':
+        rvm_system_ruby {'ruby-1.9.3-p327':
               ensure => 'present';
         }
         rvm_gem {'jekyll':
